e201f8b91d01c6b0ad0f7195533c21ca4a923e60
Greg Murray <gremurra@gmail.com>
2020-05-14 16:50:11 -0400

    Initial Migration, Registered User, Added Profile Class

---
+++ Ballpark.Data/Profile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ballpark.Data
+{
+    public class Profile
+    {
+        [Key]
+        public int ProfileID { get; set; }
+        [Required]
+        public Guid OwnerID { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Display(Name = "Favorite Team")]
+        public string FavTeam { get; set; }
+    }
+}
