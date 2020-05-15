1d58959baed820f7e1963bf359bc804fb568f69a
Greg Murray <gremurra@gmail.com>
2020-05-15 16:09:39 -0400

    Ability to create a Fan Profile

--- Ballpark.WebMVC/Controllers/ProfileController.cs
+++ Ballpark.WebMVC/Controllers/ProfileController.cs
@@ -1,4 +1,6 @@
 ﻿using Ballpark.Models;
+using Ballpark.Services;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +13,12 @@ namespace Ballpark.WebMVC.Controllers
     public class ProfileController : Controller
     {
         // GET: Profile
-        public ActionResult Index()
+        public ActionResult Index()     //displays all the notes for the current user.
         {
-            var model = new ProfileListItem[0];
+            var userID = Guid.Parse(User.Identity.GetUserId());
+            var service = new ProfileService(userID);
+            var model = service.GetProfiles();
+
             return View(model);
         }
 
@@ -30,11 +35,26 @@ namespace Ballpark.WebMVC.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(ProfileCreate model)
         {
-            if (ModelState.IsValid)
-            {
+            if (!ModelState.IsValid) return View(model);
 
-            }
+            var service = CreateProfileService();
+
+            if (service.CreateProfile(model))
+            {
+                TempData["SaveResult"] = "Your profile was created.";
+                return RedirectToAction("Index");
+            };
+
+            ModelState.AddModelError("", "Profile could not be created.");
+
             return View(model);
+        }
+
+        private ProfileService CreateProfileService()
+        {
+            var userID = Guid.Parse(User.Identity.GetUserId());
+            var service = new ProfileService(userID);
+            return service;
         }
     }
 }
