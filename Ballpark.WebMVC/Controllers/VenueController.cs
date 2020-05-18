dd6ac343fc604ec70c6f443290d86161bc2623a7
Greg Murray <gremurra@gmail.com>
2020-05-18 11:19:34 -0400

    Added Venue class, created Model Folders

---
+++ Ballpark.WebMVC/Controllers/VenueController.cs
@@ -0,0 +1,20 @@
+﻿using Ballpark.Models.Venue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ballpark.WebMVC.Controllers
+{
+    [Authorize]
+    public class VenueController : Controller
+    {
+        // GET: Venue
+        public ActionResult Index()
+        {
+            var model = new VenueListItem[0];
+            return View(model);
+        }
+    }
+}
