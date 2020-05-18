6daeab523a78a98fd5fe43fdad4d77a44d217522
Greg Murray <gremurra@gmail.com>
2020-05-18 15:47:12 -0400

    Added TeamCreate model

---
+++ Ballpark.Models/Team/TeamCreate.cs
@@ -0,0 +1,24 @@
+﻿using Ballpark.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ballpark.Models.Team
+{
+    public class TeamCreate
+    {
+        [Required]
+        [Display(Name = "Team Name")]
+        public string TeamName { get; set; }
+        [Required]
+        public Sport Sport { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        [Display(Name = "Home Venue")]
+        public int VenueID { get; set; }
+    }
+}
--- Ballpark.WebMVC/Controllers/TeamController.cs
+++ Ballpark.WebMVC/Controllers/TeamController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ballpark.Models.Team;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,32 @@ using System.Web.Mvc;
 
 namespace Ballpark.WebMVC.Controllers
 {
+    [Authorize]
     public class TeamController : Controller
     {
         // GET: Team
         public ActionResult Index()
         {
+            var model = new TeamListItem[0];
+            return View(model);
+        }
+
+        //GET: Create
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        //POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create (TeamCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
         }
     }
 }
