56e58eab5ea31cf028052448f710388f9545b2e9
Greg Murray <gremurra@gmail.com>
2020-05-18 19:15:51 -0400

    Added Event Class

---
+++ Ballpark.Data/Event.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ballpark.Data
+{
+    public class Event
+    {
+        [Key]
+        public int EventID { get; set; }
+        [Required]
+        public int ProfileID { get; set; }
+        [ForeignKey(nameof(ProfileID))]
+        public virtual Profile Profile { get; set; }
+        [Required]
+        public DateTimeOffset DateOfGame { get; set; }
+        [Required]
+        public int VenueID { get; set; }
+        [ForeignKey(nameof(VenueID))]
+        public virtual Venue Venue { get; set; }
+        [Required]
+        public int TeamID { get; set; }
+        [ForeignKey(nameof(TeamID))]
+        public virtual Team Team { get; set; }
+        [Required]
+        public string AwayTeam { get; set; }
+        [Required]
+        public string Result { get; set; }
+        public string Comments { get; set; }
+    }
+}
