057eb020fb57a664605ddd68e712359d328aaf33
Greg Murray <gremurra@gmail.com>
2020-05-19 10:03:51 -0400

    Added EventCreate, added EventListItem

---
+++ Ballpark.Models/Event/EventCreate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ballpark.Models.Event
+{
+    public class EventCreate
+    {
+        [Required]
+        public DateTimeOffset DateOfGame { get; set; }
+        [Required]
+        public string VenueName { get; set; }
+        [Required]
+        public int TeamName { get; set; }
+        [Required]
+        public string AwayTeam { get; set; }
+        [Required]
+        public string Result { get; set; }
+        [Required]
+        public string Comments { get; set; }
+    }
+}
--- Ballpark.WebMVC/Controllers/EventController.cs
+++ Ballpark.WebMVC/Controllers/EventController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ballpark.Models.Event;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,31 @@ using System.Web.Mvc;
 
 namespace Ballpark.WebMVC.Controllers
 {
+    [Authorize]
     public class EventController : Controller
     {
         // GET: Event
         public ActionResult Index()
         {
+            var model = new EventListItem[0];
+            return View(model);
+        }
+
+        // GET: Create
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(EventCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
         }
     }
 }
